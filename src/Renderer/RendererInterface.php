b984274764fb273143f12d8e1399135841845727
Designcise <designcise@gmail.com>
2020-04-01 00:23:01 +0200

    Updated renderer classes

---
+++ src/Renderer/RendererInterface.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * BitFrame Framework (https://www.bitframephp.com)
+ *
+ * @author    Daniyal Hamid
+ * @copyright Copyright (c) 2017-2020 Daniyal Hamid (https://designcise.com)
+ * @license   https://bitframephp.com/about/license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace BitFrame\Renderer;
+
+/**
+ * Interface defining required template capabilities.
+ */
+interface RendererInterface
+{
+    /**
+     * Add a namespaced template path to rendering engine.
+     *
+     * @param string $namespace
+     * @param string $path
+     */
+    public function addPath(string $namespace, string $path);
+
+    /**
+     * Render a template, optionally with parameters.
+     *
+     * Implementations MUST support the `namespace::template` naming convention,
+     * and allow omitting the filename extension.
+     *
+     * @param string $nsWithPath e.g. `namespace::template`
+     * @param array $data
+     *
+     * @return string
+     */
+    public function render(string $nsWithPath, array $data = []): string;
+
+    /**
+     * Add default parameters to use with template(s).
+     *
+     * Use this method to provide default parameters to use when a template is
+     * rendered. A parameter may be overridden by providing it when calling
+     * `render()`, or by calling this method again with a `null` value.
+     *
+     * The parameter will be specific to the template name provided (e.g.
+     * `namespace::template`). To make the parameter available to any template,
+     * pass `null` for the template name.
+     *
+     * If the default parameter existed previously, subsequent invocations with
+     * the same template name and parameter name will overwrite.
+     *
+     * @param string $templateName Name of template to which the param applies;
+     *                             use `TEMPLATE_ALL` to apply to all templates.
+     * @param array $params Key/value data pair.
+     */
+    public function addDefaults(array $params, ?string $templateName = null);
+}
