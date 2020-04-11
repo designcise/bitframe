84c136be524e16d5c347f9c309aebcd1eebb600b
Designcise <designcise@gmail.com>
2020-04-11 15:53:29 +0200

    Added test for emitting non-readable stream

--- test/Integration/SapiStreamEmitterTest.php
+++ test/Integration/SapiStreamEmitterTest.php
@@ -12,15 +12,13 @@ declare(strict_types=1);
 
 namespace BitFrame\Test\Integration;
 
-use BitFrame\Factory\HttpFactory;
-use BitFrame\Factory\HttpFactoryInterface;
-use PHPUnit\Framework\MockObject\MockObject;
+use Psr\Http\Message\ResponseInterface;
+use ReflectionObject;
+use Mockery;
 use Psr\Http\Message\StreamInterface;
-use ReflectionClass;
+use BitFrame\Factory\HttpFactory;
 use BitFrame\Emitter\SapiStreamEmitter;
 use BitFrame\Http\Message\TextResponse;
-
-use function get_class;
 
 /**
  * @covers \BitFrame\Emitter\SapiStreamEmitter
@@ -31,6 +29,11 @@ class SapiStreamEmitterTest extends AbstractSapiEmitterTest
     {
         parent::setUp();
         $this->emitter = new SapiStreamEmitter();
+    }
+
+    public function tearDown(): void
+    {
+        Mockery::close();
     }
 
     public function testEmitTextResponse()
@@ -47,17 +50,16 @@ class SapiStreamEmitterTest extends AbstractSapiEmitterTest
 
     public function testReturnsBodyWhenNotReadableButIsSeekable(): void
     {
-        $body = $this->getMockBuilder(StreamInterface::class)
-            ->onlyMethods(['isSeekable', 'isReadable', '__toString'])
-            ->getMockForAbstractClass();
-
-        $body->method('isSeekable')->willReturn(true);
-        $body->method('isReadable')->willReturn(false);
-        $body->method('__toString')->willReturn('hello');
+        $stream = HttpFactory::createStream('Hello world!');
+        $mockedStream = Mockery::mock($stream, StreamInterface::class)->makePartial();
+        $mockedStream->shouldReceive('isSeekable')->andReturn(true);
+        $mockedStream->shouldReceive('isReadable')->andReturn(false);
 
         $response = HttpFactory::createResponse()
-            ->withBody($body);
+            ->withBody($mockedStream);
 
-        $this->assertSame('hello', (string) $response->getBody());
+        ob_start();
+        $this->emitter->emit($response);
+        $this->assertSame('Hello world!', ob_get_clean());
     }
 }
