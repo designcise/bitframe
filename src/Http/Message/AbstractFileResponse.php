514526f6ab4f975a8831db7a67cf1b5d93bcc38a
Daniyal Hamid <hello@designcise.com>
2022-01-01 23:11:28 +0100

    Updated copyright notice

--- src/Http/Message/AbstractFileResponse.php
+++ src/Http/Message/AbstractFileResponse.php
@@ -4,7 +4,7 @@
  * BitFrame Framework (https://www.bitframephp.com)
  *
  * @author    Daniyal Hamid
- * @copyright Copyright (c) 2017-2021 Daniyal Hamid (https://designcise.com)
+ * @copyright Copyright (c) 2017-2022 Daniyal Hamid (https://designcise.com)
  * @license   https://bitframephp.com/about/license MIT License
  */
 
